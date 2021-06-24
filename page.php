2ab1cb4bdcfde73f87480703f41022965103a8fa
shimojo <superusedys@gmail.com>
2021-06-25 07:13:04 +0900

    pageテンプレートをテスト作成

---
+++ page.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<?php
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+?>
+     <h1><?php the_title(); ?></h1>
+     <section>
+       <?php the_content(); ?>
+     </section>
+<?php
+    endwhile;
+endif;
+?>
+<?php get_footer(); ?>
---
+++ pagetemplate-senryu.php
@@ -0,0 +1,343 @@
+<?php
+/**
+ * Template Name: 川柳用のテンプレート
+ * Description: 川柳用のテンプレート
+ */
+ ?>
+<?php get_header(); ?>
+    <div class="site-section top-image">
+        <img src="<?= get_template_directory_uri(); ?>/images/senryu3.png" alt="Image" class="img-fluid">
+    </div>
+
+    <div class="site-section pb-0">
+      <div class="container">
+        <div class="row">
+          <div class="site-section-heading text-center w-border mx-auto col-xs-12 col-sm-12 col-md-12 col-lg-12" data-aos="fade-up" data-aos-delay="100">
+            <div class="mx-auto">
+            <h4 class="font-weight-bold text-left ml-3 mr-3">あなたの投票で優勝が決まる！<br>プレゼントが当たる！<br>あなたが選ぶ○○川柳大賞！</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section pt-0">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-12 col-mg-8 col-lg-8 col-xl-8">
+            <div class="site-section">
+              <div class="container">
+                <div class="row">
+                  <p class="text-left ml-3 mr-3">「コロナ禍で気づいたこと」をテーマに五・七・五の川柳を募集しました！改めて気づいた家族や大切な人との繋がり、実際に経験したことなど、素敵な作品がたくさん寄せられました！<br><br>あなたの気に入った川柳に、ぜひ投票してみてください！</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="site-section beige pt-4 pb-4">
+              <div class="container">
+                <div class="row">
+                  <div class="site-section-heading text-center w-border mx-auto col-xs-12 col-sm-12 col-md-12 col-lg-12" data-aos="fade-up" data-aos-delay="100">
+                    <h5 class="font-weight-bold mb-2">投票期間</h5>
+                    <div class="contents-border short mt-1 mb-1"></div>
+                    <p>2021年8月8日まで</p>
+                    <h5 class="font-weight-bold mb-2">結果発表</h5>
+                    <div class="contents-border short mt-1 mb-1"></div>
+                    <p>2021年8月9日</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="site-section">
+              <div class="container">
+                <div class="row">
+                  <div class="site-section-heading text-center w-border mx-auto col-12" data-aos="fade-up" data-aos-delay="100">
+                    <h4 class="font-weight-bold mb-2">応募作品</h4>
+                    <div class="contents-border short mt-1 mb-1"></div>
+                  </div>
+                  <div class="kakejiku col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="kakejiku_outer m-0 mt-4 mb-4 mx-auto">
+                      <div class="kakejiku_inner">
+                        この場所に<br>あなたの俳句を<br>表示します
+                      </div>
+                    </div>
+                    <div class="kakejiku_episode d-none">
+                      1エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                      エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                    </div>
+                    <div class="kakejiku_name col-10 mx-auto mt-3 mb-2 text-white font-weight-bold d-none">
+                      山田 花子 (20代 女性)
+                    </div>
+                    <div class="kakejiku_button mx-auto text-center">
+                      <button class="detail mx-auto pl-3 pr-3" data-toggle="modal" data-target="#modal">詳細</button>
+                      <button class="vote mx-auto pl-3 pr-3">投票</button>
+                    </div>
+                  </div>
+                  <div class="kakejiku col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="kakejiku_outer m-0 mt-4 mb-4 mx-auto">
+                      <div class="kakejiku_inner">
+                        この場所に<br>あなたの俳句を<br>表示します
+                      </div>
+                    </div>
+                    <div class="kakejiku_episode d-none">
+                      2エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                      エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                    </div>
+                    <div class="kakejiku_name d-none">
+                      山田 花子 (30代 女性)
+                    </div>
+                    <div class="kakejiku_button mx-auto text-center">
+                      <button class="detail mx-auto pl-3 pr-3" data-toggle="modal" data-target="#modal">詳細</button>
+                      <button class="vote mx-auto pl-3 pr-3">投票</button>
+                    </div>
+                  </div>
+                  <div class="kakejiku col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="kakejiku_outer m-0 mt-4 mb-4 mx-auto">
+                      <div class="kakejiku_inner">
+                        この場所に<br>あなたの俳句を<br>表示します
+                      </div>
+                    </div>
+                    <div class="kakejiku_episode d-none">
+                      3エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                      エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                    </div>
+                    <div class="kakejiku_name d-none">
+                      山田 花子 (40代 女性)
+                    </div>
+                    <div class="kakejiku_button mx-auto text-center">
+                      <button class="detail mx-auto pl-3 pr-3" data-toggle="modal" data-target="#modal">詳細</button>
+                      <button class="vote mx-auto pl-3 pr-3">投票</button>
+                    </div>
+                  </div>
+                  <div class="kakejiku col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="kakejiku_outer m-0 mt-4 mb-4 mx-auto">
+                      <div class="kakejiku_inner">
+                        この場所に<br>あなたの俳句を<br>表示します
+                      </div>
+                    </div>
+                    <div class="kakejiku_episode d-none">
+                      4エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                      エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                    </div>
+                    <div class="kakejiku_name d-none">
+                      山田 花子 (50代 女性)
+                    </div>
+                    <div class="kakejiku_button mx-auto text-center">
+                      <button class="detail mx-auto pl-3 pr-3" data-toggle="modal" data-target="#modal">詳細</button>
+                      <button class="vote mx-auto pl-3 pr-3">投票</button>
+                    </div>
+                  </div>
+                  <div class="kakejiku col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="kakejiku_outer m-0 mt-4 mb-4 mx-auto">
+                      <div class="kakejiku_inner">
+                        この場所に<br>あなたの俳句を<br>表示します
+                      </div>
+                    </div>
+                    <div class="kakejiku_episode d-none">
+                      5エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                      エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                    </div>
+                    <div class="kakejiku_name d-none">
+                      山田 花子 (60代 女性)
+                    </div>
+                    <div class="kakejiku_button mx-auto text-center">
+                      <button class="detail mx-auto pl-3 pr-3" data-toggle="modal" data-target="#modal">詳細</button>
+                      <button class="vote mx-auto pl-3 pr-3">投票</button>
+                    </div>
+                  </div>
+                  <div class="kakejiku col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="kakejiku_outer m-0 mt-4 mb-4 mx-auto">
+                      <div class="kakejiku_inner">
+                        この場所に<br>あなたの俳句を<br>表示します
+                      </div>
+                    </div>
+                    <div class="kakejiku_episode d-none">
+                      6エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                      エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                    </div>
+                    <div class="kakejiku_name d-none">
+                      山田 花子 (70代 女性)
+                    </div>
+                    <div class="kakejiku_button mx-auto text-center">
+                      <button class="detail mx-auto pl-3 pr-3" data-toggle="modal" data-target="#modal">詳細</button>
+                      <button class="vote mx-auto pl-3 pr-3">投票</button>
+                    </div>
+                  </div>
+                  <div class="kakejiku col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="kakejiku_outer m-0 mt-4 mb-4 mx-auto">
+                      <div class="kakejiku_inner">
+                        この場所に<br>あなたの俳句を<br>表示します
+                      </div>
+                    </div>
+                    <div class="kakejiku_episode d-none">
+                      7エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                      エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                    </div>
+                    <div class="kakejiku_name d-none">
+                      山田 花子 (80代 女性)
+                    </div>
+                    <div class="kakejiku_button mx-auto text-center">
+                      <button class="detail mx-auto pl-3 pr-3" data-toggle="modal" data-target="#modal">詳細</button>
+                      <button class="vote mx-auto pl-3 pr-3">投票</button>
+                    </div>
+                  </div>
+                  <div class="kakejiku col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                    <div class="kakejiku_outer m-0 mt-4 mb-4 mx-auto">
+                      <div class="kakejiku_inner">
+                        この場所に<br>あなたの俳句を<br>表示します
+                      </div>
+                    </div>
+                    <div class="kakejiku_episode d-none">
+                      8エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                      エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。エピソード入ります。
+                    </div>
+                    <div class="kakejiku_name d-none">
+                      山田 花子 (90代 女性)
+                    </div>
+                    <div class="kakejiku_button mx-auto text-center">
+                      <button class="detail mx-auto pl-3 pr-3" data-toggle="modal" data-target="#modal">詳細</button>
+                      <button class="vote mx-auto pl-3 pr-3">投票</button>
+                    </div>
+                  </div>
+                  <div class="text-center col-12 mt-5 mb-5" data-aos="fade-up" data-aos-delay="100">
+                    <div href="#" class="arrow down circle"></div>
+                  </div>
+                  <div class="text-center col-12" data-aos="fade-up" data-aos-delay="100">
+                    <p class="text-left mt-6">投票結果は8月7日に掲載されます。</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="site-section pt-3 pb-3">
+              <div class="container">
+                <div class="row">
+                  <div class="site-section-heading text-center w-border mx-auto col-12" data-aos="fade-up" data-aos-delay="100">
+                    <h4 class="font-weight-bold mb-2">賞品</h4>
+                    <div class="contents-border short mt-1 mb-1"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="site-section beige">
+              <div class="container">
+                <div class="row">
+                  <div class="col-4 text-center mt-2">
+                    <div class="double-circle l mx-auto">
+                      <div class="circle-inner">
+                        <div class="title">最優秀賞</div>
+                        <div class="sub">1名様</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-8 mt-2">
+                    <div class="text">
+                      ・ギフトカード（1万円分）<br>
+                      ・地域の特産品
+                    </div>
+                    <div class="image">
+                      <img class="prize col-6" src="<?= get_template_directory_uri(); ?>/images/photo5.png"><img class="prize col-6" src="<?= get_template_directory_uri(); ?>/images/photo5.png">
+                    </div>
+                  </div>
+                  <div class="col-4 text-center mt-2">
+                    <div class="double-circle m mx-auto">
+                      <div class="circle-inner">
+                        <div class="title">優秀賞</div>
+                        <div class="sub">1名様</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-8 mt-2">
+                    <div class="text">
+                      ・ギフトカード（1万円分）<br>
+                      ・地域の特産品
+                    </div>
+                    <div class="image">
+                      <img class="prize col-6" src="<?= get_template_directory_uri(); ?>/images/photo5.png"><img class="prize col-6" src="<?= get_template_directory_uri(); ?>/images/photo5.png">
+                    </div>
+                  </div>
+                  <div class="col-4 text-center mt-2">
+                    <div class="double-circle s mx-auto">
+                      <div class="circle-inner">
+                        <div class="title">佳作</div>
+                        <div class="sub">8名様</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-8 mt-2">
+                    <div class="text">
+                      ・ギフトカード（1万円分）<br>
+                      ・地域の特産品
+                    </div>
+                    <div class="image">
+                      <img class="prize col-6" src="<?= get_template_directory_uri(); ?>/images/photo5.png"><img class="prize col-6" src="<?= get_template_directory_uri(); ?>/images/photo5.png">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-mg-4 col-lg-4 col-xl-4">
+            <div class="site-section">
+              <div class="container">
+                <div class="row">
+                  <div class="site-section-heading w-border mx-auto col-12" data-aos="fade-up" data-aos-delay="100">
+                    <div>
+                      <h4 class="heading-bar color-cyan font-weight-bold mb-2">その他プログラム&nbsp;</h4>
+                      <div class="team-member text-center image col-12 mt-3 mb-5">
+                        <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
+                      </div>
+                    </div>
+                    <div>
+                      <h4 class="font-weight-bold mb-3">連合会</h4>
+                      <div class="heading-bar color-gray mb-3"></div>
+                      <div class="team-member text-center image col-12 mt-3 mb-3">
+                        <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
+                      </div>
+                    </div>
+                    <div>
+                      <h4 class="font-weight-bold mb-3">MOVIEコンテスト</h4>
+                      <div class="heading-bar color-gray mb-3"></div>
+                      <div class="team-member text-center image col-12 mt-3 mb-3">
+                        <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
+                      </div>
+                    </div>
+                    <div>
+                      <h4 class="font-weight-bold mb-3">PHOTOコンテスト</h4>
+                      <div class="heading-bar color-gray mb-3"></div>
+                      <div class="team-member text-center image col-12 mt-3 mb-3">
+                        <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
+                      </div>
+                    </div>
+                    <div>
+                      <h4 class="font-weight-bold mb-3">川柳コンテスト</h4>
+                      <div class="heading-bar color-gray mb-3"></div>
+                      <div class="team-member text-center image col-12 mt-3 mb-3">
+                        <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="label1" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="kakejiku_outer m-0 mt-4 mb-4 mx-auto">
+          <div class="kakejiku_inner">
+          </div>
+        </div>
+        <div class="kakejiku_episode col-10 mx-auto mt-4 mb-3 text-white font-weight-bold">
+        </div>
+        <div class="kakejiku_name col-10 mx-auto mt-3 mb-2 text-white font-weight-bold">
+        </div>
+        <div class="kakejiku_button mx-auto text-center">
+          <button class="vote mx-auto pl-3 pr-3" data-dismiss="modal">投票</button>
+        </div>
+      </div>
+    </div>
+<?php get_footer(); ?>
