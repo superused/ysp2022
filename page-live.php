11e25e2853003d9d2a55ab158bb8b13ae9dbdc02
shimojo <shimojo@no1s.biz>
2021-06-29 18:45:08 +0900

    liveの固定ページを追加、川柳テーマを削除

---
+++ page-live.php
@@ -0,0 +1,209 @@
+<?php get_header(); ?>
+    <div class="site-section top-image pb-0" data-aos="fade-up" data-aos-delay="100">
+      <div class="container">
+        <div class="row">
+          <img src="<?= get_template_directory_uri(); ?>/images/video1.jpg" alt="Image" class="img-fluid w-100 h-100">
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section pt-4 pb-4 mt-5">
+      <div class="container">
+        <div class="row">
+          <div class="site-section-heading w-border mx-auto col-12" data-aos="fade-up" data-aos-delay="100">
+            <div>
+              <h3 class="color-cyan font-weight-bold mb-2">LIVE スケジュール</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section beige p-0" data-aos="fade-up" data-aos-delay="100">
+      <div class="container">
+        <div class="row">
+          <div class="site-section-heading text-center w-border mx-auto col-12 font-weight-bold">
+            <button class="btn btn-lg text-white bg-cyan">
+              <div class="row">
+                <div class="col-12 col-md-4 col-lg-4 col-xl-4 m-0 p-0"><span>7</span></div>
+                <div class="col-12 col-md-8 col-lg-8 col-xl-8 m-0 pl-1 pr-1 d-flex align-items-end"><span class="mx-auto small">SAT</span></div>
+              </div>
+            </button>
+            <button class="btn btn-lg border-gray bg-white">
+              <div class="row">
+                <div class="col-12 col-md-4 col-lg-4 col-xl-4 m-0 p-0"><span>8</span></div>
+                <div class="col-12 col-md-8 col-lg-8 col-xl-8 m-0 pl-1 pr-1 d-flex align-items-end"><span class="mx-auto small">SUN</span></div>
+              </div>
+            </button>
+            <button class="btn btn-lg border-gray bg-white">
+              <div class="row">
+                <div class="col-12 col-md-4 col-lg-4 col-xl-4 m-0 p-0"><span>9</span></div>
+                <div class="col-12 col-md-8 col-lg-8 col-xl-8 m-0 pl-1 pr-1 d-flex align-items-end"><span class="mx-auto small">MON</span></div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section pb-0">
+      <div class="container">
+        <div class="row">
+          <div class="site-section-heading text-center w-border mx-auto col-xs-12 col-sm-12 col-md-12 col-lg-12" data-aos="fade-up" data-aos-delay="100">
+            <div class="mx-auto">
+              <h3 class="font-weight-bold text-center ml-3 mr-3"><span class="mr-2">8/7</span><span class="small">SAT</span></h3>
+              <div class="contents-border short mt-1 mb-1"></div>
+            </div>
+            <div class="mx-auto mt-5">
+              <div class="small font-weight-bold">テーマ</div>
+              <h3 class="font-weight-bold">地域を見つめる教会</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section pt-0">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-12 col-mg-8 col-lg-8 col-xl-8">
+            <div class="site-section w-border mx-auto" data-aos="fade-up" data-aos-delay="100">
+              <h4 class="heading-bar color-cyan font-weight-bold mb-2"><span class="pr-3">LIVEプログラム</span></h4>
+              <div class="mt-4"></div>
+              <div class="live-time row bg-light-gray" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-3 font-weight-bold pt-3 pl-3">14:30</div>
+                <div class="col-6 p-0 pb-3">
+                  <img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid">
+                </div>
+                <div class="col-3"></div>
+              </div>
+              <div class="live-time row bg-light-gray" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-3 font-weight-bold m-0 p-0">
+                  <div class="bg-light2-gray w-100 h-50 pt-3 pl-3">15:00</div>
+                  <div class="bg-light-gray w-100 h-50"></div>
+                </div>
+                <div class="col-6 p-0">
+                  <img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid mb-3">
+                </div>
+                <div class="col-3 font-weight-bold m-0 p-0">
+                  <div class="bg-light2-gray w-100 h-50"></div>
+                  <div class="bg-light-gray w-100 h-50"></div>
+                </div>
+              </div>
+              <div class="live-time row bg-light-gray" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-3 font-weight-bold m-0 p-0">
+                  <div class="bg-light2-gray w-100 h-50 pt-3 pl-3">16:00</div>
+                  <div class="bg-light-gray w-100 h-50"></div>
+                </div>
+                <div class="col-6 p-0">
+                  <img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid mb-3">
+                </div>
+                <div class="col-3 font-weight-bold m-0 p-0">
+                  <div class="bg-light2-gray w-100 h-50"></div>
+                  <div class="bg-light-gray w-100 h-50"></div>
+                </div>
+              </div>
+              <div class="live-time row bg-light-gray" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-3 font-weight-bold m-0 p-0">
+                  <div class="bg-light2-gray w-100 h-50 pt-3 pl-3">17:00</div>
+                  <div class="bg-light-gray w-100 h-50"></div>
+                </div>
+                <div class="col-6 m-0 p-0">
+                  <div class="bg-light2-gray w-100 h-50"></div>
+                  <div class="bg-light-gray w-100 h-50"></div>
+                </div>
+                <div class="col-3 font-weight-bold m-0 p-0">
+                  <div class="bg-light2-gray w-100 h-50"></div>
+                  <div class="bg-light-gray w-100 h-50"></div>
+                </div>
+              </div>
+              <div class="live-time row bg-light-gray" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-3 font-weight-bold m-0 p-0">
+                  <div class="bg-light2-gray w-100 h-50 pt-3 pl-3">18:00</div>
+                  <div class="bg-light-gray w-100 h-50"></div>
+                </div>
+                <div class="col-6 p-0">
+                  <img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid mb-3">
+                </div>
+                <div class="col-3 font-weight-bold m-0 p-0">
+                  <div class="bg-light2-gray w-100 h-50"></div>
+                  <div class="bg-light-gray w-100 h-50"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-mg-4 col-lg-4 col-xl-4">
+            <div class="site-section">
+              <div class="container">
+                <div class="row">
+                  <div class="site-section-heading w-border mx-auto col-12">
+                    <div data-aos="fade-up" data-aos-delay="100">
+                      <h4 class="heading-bar color-cyan font-weight-bold mb-2"><span class="pr-3">その他プログラム</span></h4>
+                      <div class="team-member text-center image col-12 mt-3 mb-5">
+                        <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
+                      </div>
+                    </div>
+                    <div data-aos="fade-up" data-aos-delay="100">
+                      <h4 class="font-weight-bold mb-3">連合会</h4>
+                      <div class="heading-bar color-gray mb-3"></div>
+                      <div class="team-member text-center image col-12 mt-3 mb-3">
+                        <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
+                      </div>
+                    </div>
+                    <div data-aos="fade-up" data-aos-delay="100">
+                      <h4 class="font-weight-bold mb-3">MOVIEコンテスト</h4>
+                      <div class="heading-bar color-gray mb-3"></div>
+                      <div class="team-member text-center image col-12 mt-3 mb-3">
+                        <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
+                      </div>
+                    </div>
+                    <div data-aos="fade-up" data-aos-delay="100">
+                      <h4 class="font-weight-bold mb-3">PHOTOコンテスト</h4>
+                      <div class="heading-bar color-gray mb-3"></div>
+                      <div class="team-member text-center image col-12 mt-3 mb-3">
+                        <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
+                      </div>
+                    </div>
+                    <div data-aos="fade-up" data-aos-delay="100">
+                      <h4 class="font-weight-bold mb-3">川柳コンテスト</h4>
+                      <div class="heading-bar color-gray mb-3"></div>
+                      <div class="team-member text-center image col-12 mt-3 mb-3">
+                        <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section beige p-0" data-aos="fade-up" data-aos-delay="100">
+      <div class="container">
+        <div class="row">
+          <div class="site-section-heading text-center w-border mx-auto col-12 font-weight-bold">
+            <button class="btn btn-lg text-white bg-cyan">
+              <div class="row">
+                <div class="col-12 col-md-4 col-lg-4 col-xl-4 m-0 p-0"><span>7</span></div>
+                <div class="col-12 col-md-8 col-lg-8 col-xl-8 m-0 pl-1 pr-1 d-flex align-items-end"><span class="mx-auto small">SAT</span></div>
+              </div>
+            </button>
+            <button class="btn btn-lg border-gray bg-white">
+              <div class="row">
+                <div class="col-12 col-md-4 col-lg-4 col-xl-4 m-0 p-0"><span>8</span></div>
+                <div class="col-12 col-md-8 col-lg-8 col-xl-8 m-0 pl-1 pr-1 d-flex align-items-end"><span class="mx-auto small">SUN</span></div>
+              </div>
+            </button>
+            <button class="btn btn-lg border-gray bg-white">
+              <div class="row">
+                <div class="col-12 col-md-4 col-lg-4 col-xl-4 m-0 p-0"><span>9</span></div>
+                <div class="col-12 col-md-8 col-lg-8 col-xl-8 m-0 pl-1 pr-1 d-flex align-items-end"><span class="mx-auto small">MON</span></div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php get_footer(); ?>
