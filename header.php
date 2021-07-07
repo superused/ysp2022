3071c0c166017867d8c272a148468369ef465ff0
shimojo <superusedys@gmail.com>
2021-07-08 02:09:54 +0900

    モバイル版ハンバーガーメニューのデザイン追加

--- header.php
+++ header.php
@@ -63,42 +63,90 @@
       <div class="site-navbar">
         <div class="container py-1">
           <div class="row align-items-center">
-            <div class="col-8 col-md-8 col-lg-3 col-xl-3">
+            <div class="col-8 col-md-8 col-lg-2 col-xl-2">
               <h1 class="logo-header mb-0">
               <a href="<?= home_url();?>" class="text-white h2 mb-0">
                   <img src="<?= get_template_directory_uri(); ?>/images/logo1.png" alt="Image" class="mx-auto">
                 </a>
               </h1>
             </div>
-            <div class="col-4 col-md-4 col-lg-9 col-xl-9">
+            <div class="col-4 col-md-4 col-lg-10 col-xl-10">
               <nav class="site-navigation text-right text-md-right" role="navigation">
 
                 <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h2"></span></a></div>
 
                 <ul class="site-menu js-clone-nav d-none d-lg-block">
-                  <li class="active">
-                    <a href="">Home</a>
-                  </li>
                   <li class="has-children">
-                    <a href="<?= esc_url(home_url('/live/'));?>">LIVE</a>
-                    <ul class="dropdown arrow-top">
-                      <li><a href="<?= esc_url(home_url('/live2/'));?>">LIVE2</a></li>
-                      <li><a href="#">LIVE2</a></li>
-                      <li><a href="#">LIVE3</a></li>
-                      <li class="has-children">
-                        <a href="#">サブメニュー</a>
-                        <ul class="dropdown">
-                          <li><a href="#">サブ1</a></li>
-                          <li><a href="#">サブ2</a></li>
-                          <li><a href="#">サブ3</a></li>
-                        </ul>
+                    <a href="javascript:void(0);"><img src="<?= get_template_directory_uri(); ?>/images/icon_contents.png"><span>コンテンツ</span></a>
+                    <ul class="dropdown">
+                      <li>
+                        <a href="<?= esc_url(home_url('/live2/'));?>">
+                          <img src="<?= get_template_directory_uri(); ?>/images/icon_live.png">
+                          <span>LIVE</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <img src="<?= get_template_directory_uri(); ?>/images/icon_project.png">
+                          <span>プロジェクト</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="<?= esc_url(home_url('/photo-contest/'));?>">
+                          <img src="<?= get_template_directory_uri(); ?>/images/icon_photo.png">
+                          <span>フォトコンテスト</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="<?= esc_url(home_url('/movie-contest/'));?>">
+                          <img src="<?= get_template_directory_uri(); ?>/images/icon_movie.png">
+                          <span>ムービーコンテスト</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="<?= esc_url(home_url('/senryu/'));?>">
+                          <img src="<?= get_template_directory_uri(); ?>/images/icon_senryu.png">
+                          <span>川柳コンテスト</span>
+                        </a>
                       </li>
                     </ul>
                   </li>
-                  <li><a href="<?= esc_url(home_url('/union/'));?>">連合会一覧</a></li>
-                  <li><a href="<?= esc_url(home_url('/video-contest/'));?>">ビデオコンテスト</a></li>
-                  <li><a href="<?= esc_url(home_url('/photo-contest/'));?>">フォトコンテスト</a></li>
-                  <li><a href="<?= esc_url(home_url('/senryu/'));?>">川柳</a></li>
+                  <li>
+                    <a href="<?= esc_url(home_url('/union/'));?>">
+                      <img src="<?= get_template_directory_uri(); ?>/images/icon_present.png">
+                      <span>プレゼントアンケート</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="<?= esc_url(home_url('/video-contest/'));?>">
+                      <img src="<?= get_template_directory_uri(); ?>/images/icon_news.png">
+                      <span>NEWS</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="<?= esc_url(home_url('/photo-contest/'));?>">
+                      <img src="<?= get_template_directory_uri(); ?>/images/icon_about.png">
+                      <span>aboutフェス</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="<?= esc_url(home_url('/'));?>">
+                      <img src="<?= get_template_directory_uri(); ?>/images/icon_top.png">
+                      <span>TOPページ</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="<?= esc_url(home_url('/contact/'));?>">
+                      <img src="<?= get_template_directory_uri(); ?>/images/icon_contact.png">
+                      <span>CONTACT</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <img src="<?= get_template_directory_uri(); ?>/images/icon_goods.png">
+                      <span>グッズショップ（外部リンク）</span>
+                    </a>
+                  </li>
                 </ul>
               </nav>
             </div>
@@ -109,11 +157,13 @@
 
     <div class="site-mobile-menu">
       <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
+        <div class="site-mobile-menu-close mb-3">
+          <span class="icon-close2 js-menu-toggle text-cyan"></span>
         </div>
       </div>
-      <div class="site-mobile-menu-body"></div>
+      <div class="site-mobile-menu-body">
+        <div class="font-weight-bold">OFFICIAL SNS</div>
+      </div>
     </div> <!-- .site-mobile-menu -->
 
     <!-- ヘッダー共通処理 end -->
--- js/app.js
+++ js/app.js
@@ -10,29 +10,8 @@ $(function() {
 
     $('.js-clone-nav').each(function() {
       var $this = $(this);
-      $this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+      $this.clone().attr('class', 'site-nav-wrap').prependTo('.site-mobile-menu-body');
     });
-    setTimeout(function() {
-      var counter = 0;
-      $('.site-mobile-menu .has-children').each(function(){
-        var $this = $(this);
-        $this.prepend('<span class="arrow-collapse collapsed">');
-
-        $this.find('.arrow-collapse').attr({
-          'data-toggle' : 'collapse',
-          'data-target' : '#collapseItem' + counter,
-        });
-
-        $this.find('> ul').attr({
-          'class' : 'collapse',
-          'id' : 'collapseItem' + counter,
-        });
-
-        counter++;
-
-      });
-
-    }, 1000);
 
     $('body').on('click', '.arrow-collapse', function(e) {
       var $this = $(this);
