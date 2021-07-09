0206f2864004dd9021b9199ee3ac87b4a3323afc
shimojo <shimojo@no1s.biz>
2021-07-09 18:01:48 +0900

    エラー用のページをカスタマイズ

--- 404.php
+++ 404.php
@@ -1,9 +1,8 @@
-<?php get_header();?>
-
-<div class="site-section">
-  <div class="container">
-    <p class="m-5">お探しのページは見つかりませんでした。</p>
+<?php include 'header-error.php'; ?>
+  <div class="site-section">
+    <div class="container" style="text-align: center;">
+      <h2 class="m-5">お探しのページは見つかりませんでした。</h2>
+      <p class="m-5">既にページが削除されてしまったか、URLを間違えている可能性があります。</p>
+    </div>
   </div>
-</div>
-
-<?php get_footer(); ?>
+<?php include 'footer-error.php'; ?>
---
+++ footer-error.php
@@ -0,0 +1,13 @@
+    <footer class="site-footer">
+      <div class="container pt-5">
+        <div class="row">
+          <div class="text-center mb-5 col-12"><img src="<?= get_template_directory_uri(); ?>/images/logo1.png" width="150"></div>
+        </div>
+        <div class="text-center credit mx-auto mb-4">
+          <span>©Peace Designer FES</span>
+        </div>
+      </div>
+    </footer>
+    <?php wp_footer();?>
+  </body>
+</html>
