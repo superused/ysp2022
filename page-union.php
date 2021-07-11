0cc93e430d4d228d7f28e5f927256710447aca52
shimojo <superusedys@gmail.com>
2021-07-12 01:19:09 +0900

    UNIONページ修正

--- page-union.php
+++ page-union.php
@@ -22,113 +22,113 @@
 
     <div class="site-section pt-0">
       <div class="container">
-        <div class="row">
-          <div class="col-12 col-sm-12 col-mg-8 col-lg-8 col-xl-8">
+        <div class="row justify-content-center">
+          <div class="col-12 col-lg-8">
             <div class="site-section">
               <div class="container">
-                <div class="row shadow w-100 mx-auto mb-4" data-aos="fade-up" data-aos-delay="100">
-                  <div class="col-12 col-lg-5 col-xl-5 p-3">
+                <div class="project-frame row w-100 mx-auto mb-5" data-aos="fade-up" data-aos-delay="100">
+                  <div class="col-12 col-md-5 p-0 mb-3">
                     <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
                   </div>
-                  <div class="col-12 col-lg-7 col-xl-7 p-3">
-                    <div class="site-section-heading w-border mx-auto col-12 mb-4 p-0">
+                  <div class="col-12 col-md-7 p-0">
+                    <div class="site-section-heading w-border mx-auto col-12 p-0 ml-md-4">
                       <h6 class="font-weight-bold mb-2">8/7(Sat) 15:00 ～ 16:00</h6>
                       <div class="contents-border mt-1 mb-1 text-left w-50 ml-0"></div>
                       <h6 class="font-weight-bold mb-2">連合会名</h6>
                       <div class="small pb-2 mb-2">
                         テキスト入ります。テキスト入ります。
                       </div>
-                      <div class="union-box">
+                      <div class="union-box text-center text-md-left">
                         <a href="#"><button class="btn btn-show type1 mx-auto" ontouchstart="">詳しく見る</button></a>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="row shadow w-100 mx-auto mb-4" data-aos="fade-up" data-aos-delay="100">
-                  <div class="col-12 col-lg-5 col-xl-5 p-3">
+                <div class="project-frame row w-100 mx-auto mb-5" data-aos="fade-up" data-aos-delay="100">
+                  <div class="col-12 col-md-5 p-0 mb-3">
                     <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
                   </div>
-                  <div class="col-12 col-lg-7 col-xl-7 p-3">
-                    <div class="site-section-heading w-border mx-auto col-12 mb-4 p-0">
+                  <div class="col-12 col-md-7 p-0">
+                    <div class="site-section-heading w-border mx-auto col-12 p-0 ml-md-4">
                       <h6 class="font-weight-bold mb-2">8/7(Sat) 15:00 ～ 16:00</h6>
                       <div class="contents-border mt-1 mb-1 text-left w-50 ml-0"></div>
                       <h6 class="font-weight-bold mb-2">連合会名</h6>
                       <div class="small pb-2 mb-2">
                         テキスト入ります。テキスト入ります。
                       </div>
-                      <div class="union-box">
+                      <div class="union-box text-center text-md-left">
                         <a href="#"><button class="btn btn-show type1 mx-auto" ontouchstart="">詳しく見る</button></a>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="row shadow w-100 mx-auto mb-4" data-aos="fade-up" data-aos-delay="100">
-                  <div class="col-12 col-lg-5 col-xl-5 p-3">
+                <div class="project-frame row w-100 mx-auto mb-5" data-aos="fade-up" data-aos-delay="100">
+                  <div class="col-12 col-md-5 p-0 mb-3">
                     <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
                   </div>
-                  <div class="col-12 col-lg-7 col-xl-7 p-3">
-                    <div class="site-section-heading w-border mx-auto col-12 mb-4 p-0">
+                  <div class="col-12 col-md-7 p-0">
+                    <div class="site-section-heading w-border mx-auto col-12 p-0 ml-md-4">
                       <h6 class="font-weight-bold mb-2">8/7(Sat) 15:00 ～ 16:00</h6>
                       <div class="contents-border mt-1 mb-1 text-left w-50 ml-0"></div>
                       <h6 class="font-weight-bold mb-2">連合会名</h6>
                       <div class="small pb-2 mb-2">
                         テキスト入ります。テキスト入ります。
                       </div>
-                      <div class="union-box">
+                      <div class="union-box text-center text-md-left">
                         <a href="#"><button class="btn btn-show type1 mx-auto" ontouchstart="">詳しく見る</button></a>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="row shadow w-100 mx-auto mb-4" data-aos="fade-up" data-aos-delay="100">
-                  <div class="col-12 col-lg-5 col-xl-5 p-3">
+                <div class="project-frame row w-100 mx-auto mb-5" data-aos="fade-up" data-aos-delay="100">
+                  <div class="col-12 col-md-5 p-0 mb-3">
                     <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
                   </div>
-                  <div class="col-12 col-lg-7 col-xl-7 p-3">
-                    <div class="site-section-heading w-border mx-auto col-12 mb-4 p-0">
+                  <div class="col-12 col-md-7 p-0">
+                    <div class="site-section-heading w-border mx-auto col-12 p-0 ml-md-4">
                       <h6 class="font-weight-bold mb-2">8/7(Sat) 15:00 ～ 16:00</h6>
                       <div class="contents-border mt-1 mb-1 text-left w-50 ml-0"></div>
                       <h6 class="font-weight-bold mb-2">連合会名</h6>
                       <div class="small pb-2 mb-2">
                         テキスト入ります。テキスト入ります。
                       </div>
-                      <div class="union-box">
+                      <div class="union-box text-center text-md-left">
                         <a href="#"><button class="btn btn-show type1 mx-auto" ontouchstart="">詳しく見る</button></a>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="row shadow w-100 mx-auto mb-4" data-aos="fade-up" data-aos-delay="100">
-                  <div class="col-12 col-lg-5 col-xl-5 p-3">
+                <div class="project-frame row w-100 mx-auto mb-5" data-aos="fade-up" data-aos-delay="100">
+                  <div class="col-12 col-md-5 p-0 mb-3">
                     <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
                   </div>
-                  <div class="col-12 col-lg-7 col-xl-7 p-3">
-                    <div class="site-section-heading w-border mx-auto col-12 mb-4 p-0">
+                  <div class="col-12 col-md-7 p-0">
+                    <div class="site-section-heading w-border mx-auto col-12 p-0 ml-md-4">
                       <h6 class="font-weight-bold mb-2">8/7(Sat) 15:00 ～ 16:00</h6>
                       <div class="contents-border mt-1 mb-1 text-left w-50 ml-0"></div>
                       <h6 class="font-weight-bold mb-2">連合会名</h6>
                       <div class="small pb-2 mb-2">
                         テキスト入ります。テキスト入ります。
                       </div>
-                      <div class="union-box">
+                      <div class="union-box text-center text-md-left">
                         <a href="#"><button class="btn btn-show type1 mx-auto" ontouchstart="">詳しく見る</button></a>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="row shadow w-100 mx-auto mb-4" data-aos="fade-up" data-aos-delay="100">
-                  <div class="col-12 col-lg-5 col-xl-5 p-3">
+                <div class="project-frame row w-100 mx-auto mb-5" data-aos="fade-up" data-aos-delay="100">
+                  <div class="col-12 col-md-5 p-0 mb-3">
                     <a href="#"><img src="<?= get_template_directory_uri(); ?>/images/photo5.png" alt="Image" class="img-fluid"></a>
                   </div>
-                  <div class="col-12 col-lg-7 col-xl-7 p-3">
-                    <div class="site-section-heading w-border mx-auto col-12 mb-4 p-0">
+                  <div class="col-12 col-md-7 p-0">
+                    <div class="site-section-heading w-border mx-auto col-12 p-0 ml-md-4">
                       <h6 class="font-weight-bold mb-2">8/7(Sat) 15:00 ～ 16:00</h6>
                       <div class="contents-border mt-1 mb-1 text-left w-50 ml-0"></div>
                       <h6 class="font-weight-bold mb-2">連合会名</h6>
                       <div class="small pb-2 mb-2">
                         テキスト入ります。テキスト入ります。
                       </div>
-                      <div class="union-box">
+                      <div class="union-box text-center text-md-left">
                         <a href="#"><button class="btn btn-show type1 mx-auto" ontouchstart="">詳しく見る</button></a>
                       </div>
                     </div>
