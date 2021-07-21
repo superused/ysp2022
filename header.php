7bc601339a89d36245031e525c95f96e2a86c82e
shimojo <superusedys@gmail.com>
2021-07-22 00:20:42 +0900

    PC版のヘッダーの見せ方を変えた

--- header.php
+++ header.php
@@ -70,37 +70,43 @@
                 <div class="d-inline-block d-lg-none ml-md-0 mr-auto p-0"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h2"></span></a></div>
 
                 <ul class="site-menu js-clone-nav d-none d-lg-block text-nowrap">
+                  <li>
+                    <a href="<?= HOME_URL;?>">
+                      <img src="<?= get_template_directory_uri(); ?>/images/icon_top.png">
+                      <div>TOPページ</div>
+                    </a>
+                  </li>
                   <li class="has-children">
-                    <a href="javascript:void(0);"><img src="<?= get_template_directory_uri(); ?>/images/icon_contents.png"><span>コンテンツ</span></a>
+                    <a href="javascript:void(0);"><img src="<?= get_template_directory_uri(); ?>/images/icon_contents.png"><div>コンテンツ</div></a>
                     <ul class="dropdown">
                       <li>
                         <a href="<?= LIVE_CONTENTS_URL;?>" class="inpage-link">
                           <img src="<?= get_template_directory_uri(); ?>/images/icon_live.png">
-                          <span>LIVE</span>
+                          <div>LIVE</div>
                         </a>
                       </li>
                       <li>
                         <a href="<?= PROJECT_URL;?>" class="inpage-link">
                           <img src="<?= get_template_directory_uri(); ?>/images/icon_project.png">
-                          <span>プロジェクト</span>
+                          <div>プロジェクト</div>
                         </a>
                       </li>
                       <li>
                         <a href="<?= PHOTO_CONTEST_URL;?>">
                           <img src="<?= get_template_directory_uri(); ?>/images/icon_photo.png">
-                          <span>フォトコンテスト</span>
+                          <div>フォトコンテスト</div>
                         </a>
                       </li>
                       <li>
                         <a href="<?= MOVIE_CONTEST_URL;?>">
                           <img src="<?= get_template_directory_uri(); ?>/images/icon_movie.png">
-                          <span>ムービーコンテスト</span>
+                          <div>ムービーコンテスト</div>
                         </a>
                       </li>
                       <li>
                         <a href="<?= SENRYU_CONTEST_URL;?>">
                           <img src="<?= get_template_directory_uri(); ?>/images/icon_senryu.png">
-                          <span>川柳コンテスト</span>
+                          <div>川柳コンテスト</div>
                         </a>
                       </li>
                     </ul>
@@ -108,37 +114,31 @@
                   <li>
                     <a href="<?= QUESTIONNAIRE_URL;?>">
                       <img src="<?= get_template_directory_uri(); ?>/images/icon_present.png">
-                      <span>アンケート＆プレゼント</span>
+                      <div>アンケート＆プレゼント</div>
                     </a>
                   </li>
                   <li>
                     <a href="<?= NEWS_URL;?>">
                       <img src="<?= get_template_directory_uri(); ?>/images/icon_news.png">
-                      <span>最新情報</span>
+                      <div>最新情報</div>
                     </a>
                   </li>
                   <li>
                     <a href="<?= CONCEPT_URL;?>">
                       <img src="<?= get_template_directory_uri(); ?>/images/icon_about.png">
-                      <span>Peace designer Fes とは</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="<?= HOME_URL;?>">
-                      <img src="<?= get_template_directory_uri(); ?>/images/icon_top.png">
-                      <span>TOPページ</span>
+                      <div>Peace designer Fes とは</div>
                     </a>
                   </li>
                   <li>
                     <a href="<?= CONTACT_URL;?>">
                       <img src="<?= get_template_directory_uri(); ?>/images/icon_contact.png">
-                      <span>お問い合わせ</span>
+                      <div>お問い合わせ</div>
                     </a>
                   </li>
                   <li>
                     <a href="<?= GOODS_SHOP_URL; ?>">
                       <img src="<?= get_template_directory_uri(); ?>/images/icon_goods.png">
-                      <span>グッズショップ（外部リンク）</span>
+                      <div>グッズショップ（外部リンク）</div>
                     </a>
                   </li>
                   <li class="d-lg-none">
