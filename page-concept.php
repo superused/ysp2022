dc75ac9fca6dcecd617582579983cbbbc12881e4
shimojo <shimojo@no1s.biz>
2021-07-26 18:56:27 +0900

    conceptページPCを版修正

--- page-concept.php
+++ page-concept.php
@@ -1,21 +1,24 @@
 <?php get_header(); ?>
 
     <div class=""></div>
-    <div class="site-section pt-0">
-      <div class="container">
+    <div class="site-section-heading">
+      <div class="container-fluid">
         <div class="row justify-content-center">
-          <div class="site-margin-top-section col-12 col-md-8 bg-concept text-white pb-5">
-            <div class="site-section pb-0">
+          <div class="site-margin-top-section col-12 bg-concept text-white pb-5">
+            <div class="site-section rem-md-1-1 pb-0">
               <div class="container">
                 <div class="row">
                   <div class="site-section-heading text-center w-border mx-auto col-12" data-aos="fade-up" data-aos-delay="100">
                     <div class="mx-auto">
-                      <h1 class="text-white text-center mx-3">CONCEPT</h1>
-                      <p class="mb-5">Peace designer Fes 開催によせる想い</p>
+                      <div><h1 class="text-white text-center mx-3 top-subtitle">CONCEPT</h1></div>
+                      <div><h2 class="concept-subtitle mb-5 text-white x-small font-weight-normal font-family-none">Peace designer Fes 開催によせる想い</h2></div>
                       <div>
                         <h2 class="rem1-3 text-white font-weight-bold mb-2 small">つながりが描く私たちの未来</h2>
+                        <div class="contest-values py-4 d-none d-md-block">
+                          <img src="<?= get_template_directory_uri(); ?>/images/logo/bg/top_pc1.jpg" class="img-fluid">
+                        </div>
                         <p>東京オリンピックは史上初の無観客開催。<br>旅行やイベントにも行けず、毎日マスクしてソーシャルディスタンス。<br><br>コロナ禍では夢をあきらめるしかない…そんな青年の声も耳にします。<br><br> 気がつけば、人と人、地域と若手、日本と世界...<br>私たちはつながりを失いかけていました。<br><br>出会いやつながりは、青年が夢を見つけ、地域を活性化するきっかけでもあります。<br>こんなときだからこそ、出会いやつながりを止めたくない！<br><br>そんな想いで始まったのが、オンラインで参加型のPeace designer Fesです！<br><br>「自分の住んでいる町を元気にしたい！」<br>「希望にあふれた日本をつくりたい！」<br><br>そんな熱い人たちや、町の魅力との出会いをお届けします。<br><br>彼らとのつながりが、あなたの未来を明るくすることを願って。</p>
-                        <h2 class="rem1-3 text-white font-weight-bold pb-2 pt-3 small">Peace designer って？</h2>
+                        <h2 class="rem1-3 text-white font-weight-bold pb-2 pt-3 pt-md-5 small">Peace designer って？</h2>
                         <p>「Peace」は、みんなで思い描く幸せの<br>"青写真"。<br><br>みんなが少しずつデザインした<br>「Peace」を持ち寄り、つなぎ合わせることで<br>平和というパズルは完成します。<br><br>「Peace designer」は、<br>身近な人の幸せを想い描いて行動する人々の<br>シンボルマークです。</p>
                       </div>
                       <div class="concept-img">
@@ -27,9 +30,6 @@
                 </div>
               </div>
             </div>
-          </div>
-          <div class="col-12 col-md-4">
-            <?= get_sidebar();?>
           </div>
         </div>
       </div>
