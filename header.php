173b9d1662a7898e4579c7d56358a2f17132a429
shimojo <superusedys@gmail.com>
2021-07-30 09:53:45 +0900

    css, jsのバージョン設定

--- header.php
+++ header.php
@@ -44,7 +44,7 @@ $image = wp_get_attachment_image_src($image_id, 'full');
     <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/owl.carousel.min.css">
     <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/owl.theme.default.min.css">
     <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/aos.css">
-    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/style.css">
+    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/style.css?20210730">
     <style>
 .site-blocks-cover.overlay.top {
     background-image: url('<?= get_template_directory_uri(); ?>/images/logo/bg/dummy.png');
--- parts/footer-base.php
+++ parts/footer-base.php
@@ -32,7 +32,7 @@
     <script src="<?= get_template_directory_uri(); ?>/js/jquery.countdown.min.js"></script>
     <script src="<?= get_template_directory_uri(); ?>/js/jquery.magnific-popup.min.js"></script>
     <script src="<?= get_template_directory_uri(); ?>/js/aos.js"></script>
-    <script src="<?= get_template_directory_uri(); ?>/js/app.js"></script>
+    <script src="<?= get_template_directory_uri(); ?>/js/app.js?20210730"></script>
     <?php wp_footer();?>
   </body>
 </html>
