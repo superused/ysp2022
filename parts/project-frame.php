8db095e703f6c6fea38cd59eaa4fb894ac352172
shimojo <shimojo@no1s.biz>
2021-08-05 09:30:15 +0900

    プロジェクトでうまく動かない部分を修正

--- parts/project-frame.php
+++ parts/project-frame.php
@@ -8,11 +8,11 @@
                       </div>
                       <div class="col-12 p-0">
                         <div class="site-section-heading w-border mx-auto col-12 p-0">
-                        <h5 class="font-weight-bold mb-2"><?= the_title(); ?></h5>
+                        <h5 class="font-weight-bold mb-2"><?php the_title(); ?></h5>
                           <h6 class="font-weight-bold mb-2"><?= $datas[$postName]->union_name;?></h6>
                           <div class="contents-border mt-1 mb-1 text-left w-100 ml-0"></div>
-                          <div class="description small mb-3"><?php nl2br($datas[$postName]->description); ?></div>
-                          <div class="union-box text-center text-md-left">
+                          <div class="description small mb-3"><?= nl2br($datas[$postName]->description); ?></div>
+                          <div class="union-box text-center">
                             <a href="<?= get_permalink(); ?>" ontouchstart=""><button class="btn btn-show mx-auto" ontouchstart="">詳しく見る</button></a>
                           </div>
                         </div>
