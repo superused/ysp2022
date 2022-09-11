25425c6dac9cd44239f588b628c387b1a03d0f1b
shimojo <superusedys@gmail.com>
2022-09-12 08:37:59 +0900

    test

--- parts/fixed-menu.php
+++ parts/fixed-menu.php
@@ -4,33 +4,36 @@
           <div class="fixed-button-content">
             <a href="<?= LIVE_URL; ?>" ontouchstart="">
               <div class="contents">
-                <img src="<?= get_template_directory_uri(); ?>/images/fixed_live_button.png" alt="Image" class="img-fluid">
+top
               </div>
             </a>
           </div>
           <div class="fixed-button-content">
-            <a href="<?= PROJECT_URL; ?>" ontouchstart="">
+            <a href="<?= LIVE_URL; ?>" ontouchstart="">
               <div class="contents">
-                <img src="<?= get_template_directory_uri(); ?>/images/fixed_project_button.png" alt="Image" class="img-fluid">
+プロジェクト
               </div>
             </a>
           </div>
           <div class="fixed-button-content">
-            <a href="<?= CONTEST_URL; ?>" ontouchstart="">
+            <a href="<?= LIVE_URL; ?>" ontouchstart="">
               <div class="contents">
-                <img src="<?= get_template_directory_uri(); ?>/images/fixed_contest_button.png" alt="Image" class="img-fluid">
+問い合わせ
               </div>
             </a>
           </div>
-        </div>
-      </div>
-    </div>
-    <div class="site-section fixed-menu fixed-menu-pc zindex-tooltip m-0 p-0">
-      <div class="container-fluid m-0 p-0">
-        <div class="fixed-button-present">
           <div class="fixed-button-content">
-            <a href="<?= QUESTIONNAIRE_URL; ?>" class="ml-1 mr-2" ontouchstart="" target="_blank">
-              <img src="<?= get_template_directory_uri(); ?>/images/fixed_present.png" alt="Image" class="img-fluid">
+            <a href="<?= LIVE_URL; ?>" ontouchstart="">
+              <div class="contents">
+寄付する
+              </div>
+            </a>
+          </div>
+          <div class="fixed-button-content">
+            <a href="<?= LIVE_URL; ?>" ontouchstart="">
+              <div class="contents">
+ALL
+              </div>
             </a>
           </div>
         </div>
