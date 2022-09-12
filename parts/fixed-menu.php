803f5dff1b16a197aa69da9d54fe5e0d0acb5574
shimojo <superusedys@gmail.com>
2022-09-13 08:33:30 +0900

    メニュー編集

--- parts/fixed-menu.php
+++ parts/fixed-menu.php
@@ -4,35 +4,40 @@
           <div class="fixed-button-content">
             <a href="<?= LIVE_URL; ?>" ontouchstart="">
               <div class="contents">
-top
+                <img src="<?= get_template_directory_uri(); ?>/images/ysp/logo_circle.png" class="w-75 p-1">
+                <div class="text">TOP</div>
               </div>
             </a>
           </div>
           <div class="fixed-button-content">
             <a href="<?= LIVE_URL; ?>" ontouchstart="">
               <div class="contents">
-プロジェクト
+                <img src="<?= get_template_directory_uri(); ?>/images/ysp/ticket-star.png" class="w-75 p-1">
+                <div class="text">プロジェクト</div>
               </div>
             </a>
           </div>
           <div class="fixed-button-content">
             <a href="<?= LIVE_URL; ?>" ontouchstart="">
               <div class="contents">
-問い合わせ
+                <img src="<?= get_template_directory_uri(); ?>/images/ysp/search.png" class="w-75 p-1">
+                <div class="text">問い合わせ</div>
               </div>
             </a>
           </div>
-          <div class="fixed-button-content">
+          <div class="fixed-button-content bg-orange">
             <a href="<?= LIVE_URL; ?>" ontouchstart="">
               <div class="contents">
-寄付する
+                <img src="<?= get_template_directory_uri(); ?>/images/ysp/donate.png" class="w-75 p-1">
+                <div class="text text-white">寄付する</div>
               </div>
             </a>
           </div>
-          <div class="fixed-button-content">
+          <div class="fixed-button-content bg-cyan">
             <a href="<?= LIVE_URL; ?>" ontouchstart="">
               <div class="contents">
-ALL
+                <img src="<?= get_template_directory_uri(); ?>/images/ysp/hamburger.png" class="w-75 p-1">
+                <div class="text text-white">ALL</div>
               </div>
             </a>
           </div>
