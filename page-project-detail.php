649a8798f342c80d63bd877dd04ca99a105e6397
shimojo <shimojo@no1s.biz>
2022-10-14 02:03:33 +0900

    各プロジェクト表示ページ作成

---
+++ page-project-detail.php
@@ -0,0 +1,127 @@
+<?php get_header(); ?>
+    <div class="container-pc mb-5">
+      <img src="<?= IMG_DIR; ?>/project/1-宮城.jpg" class="mx-auto w-100">
+    </div>
+
+    <div class="site-section site-section-sm">
+      <div class="container p-md-0">
+        <div class="title-icon normal mb-6">活動紹介</div>
+        <div class="project-title">
+          <div class="project-id">
+            <div class="word">PROJECT</div>
+            <div class="number">01</div>
+          </div>
+          <div class="title">NONOSTYLE</div>
+        </div>
+        <div class="project container md">
+          <p class="explain">NONOSTYLEは、カメラやアートなどを趣味に持つ青年の「自分たちが住んでいる宮城県を盛り上げていきたい」という思いから構成されたデザインプロジェクトチームです。<br>宮城県のお店やタレントといった地域のために頑張る方々を、より多くの人に認知してもらうために無償で撮影を行い、写真を提供しています。またNONOSTYLE自身のインスタグラムでは撮影した写真や、若手の学生クリエイターと共同で制作した作品を投稿するといった情報発信を行っています。</p>
+          <div class="profile">
+            <div class="row">
+              <div class="title"><span>団体名</span></div>
+              <div class="text">nonostyle</div>
+            </div>
+            <div class="row">
+              <div class="title"><span>開催場所</span></div>
+              <div class="text">宮城県のカメラスタジオ、地元団体の活動場所</div>
+            </div>
+            <div class="row">
+              <div class="title"><span>リンク</span></div>
+              <div class="text">https://instagram.com/n0n0style</div>
+            </div>
+            <div class="row">
+              <div class="title"><span>問い合わせ</span></div>
+              <div class="text">instagramよりDMください！<br>https://instagram.com/n0n0style<br>メールアドレス<br>miyagi.nonostyle@gmail.com</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-lightgray my-6">
+      <div class="circle-bg gray start"></div>
+      <div class="circle-bg white start"></div>
+      <div class="container">
+        <div class="title-icon normal mb-5">活動の成果</div>
+        <p class="font-weight-bold small">現在までに170回以上の撮影活動を行っています。お店屋さんの紹介撮影やお笑い劇場といったイベント撮影活動においては1～3人、背景紙を使った個性あふれる写真撮影においては8～9人で撮影活動を行っています。<br>撮影した写真は、お店の広告写真や活動紹介として幅広く使われています。また、今まで撮影させていただいたお店屋さんやタレント、服飾学生と協力し、コラボ作品の撮影や宮城県の魅力を認知してもらうために、YSP宮城主催のマルシェを開催しました。</p>
+      </div>
+      <div class="circle-bg gray end"></div>
+      <div class="circle-bg white end"></div>
+    </div>
+
+    <div class="site-section site-section-sm">
+      <div class="container p-md-0">
+        <div class="title-icon normal mb-5">参加者の声</div>
+        <div class="project-voice rb">
+          <div class="image">
+            <img src="<?= IMG_DIR; ?>/voice_face/woman_3.png">
+          </div>
+          <div class="fukidashi">
+            <div class="text">
+              <div>服飾を学んでいる専門学生や、美容に関心のある学生とコラボしての作品撮影を行っています。後送段階から打ち合わせを重ね、撮影小物などもみんなで一緒に作っています。交流しながら撮影できているのが嬉しいです。学生との交流は、社会人の私にとっても非常に刺激となりました。また、撮影した作品を喜んでくれることもやりがいの1つです。これからもコミュニケーションをよくとっていきながら、活動の幅が広がればと思います。</div>
+            </div>
+          </div>
+        </div>
+        <div class="title-icon normal mb-5">地域の方の声</div>
+        <div class="project-voice lt">
+          <div class="image">
+            <img src="<?= IMG_DIR; ?>/voice_face/man_office.png">
+          </div>
+          <div class="fukidashi">
+            <div class="text">
+              <div>服飾を学んでいる専門学生や、美容に関心のある学生とコラボしての作品撮影を行っています。後送段階から打ち合わせを重ね、撮影小物などもみんなで一緒に作っています。交流しながら撮影できているのが嬉しいです。学生との交流は、社会人の私にとっても非常に刺激となりました。また、撮影した作品を喜んでくれることもやりがいの1つです。これからもコミュニケーションをよくとっていきながら、活動の幅が広がればと思います。</div>
+            </div>
+          </div>
+        </div>
+        <div class="project-voice rb">
+          <div class="image">
+            <img src="<?= IMG_DIR; ?>/voice_face/man_shop.png">
+          </div>
+          <div class="fukidashi">
+            <div class="text">
+              <div>服飾を学んでいる専門学生や、美容に関心のある学生とコラボしての作品撮影を行っています。後送段階から打ち合わせを重ね、撮影小物などもみんなで一緒に作っています。交流しながら撮影できているのが嬉しいです。学生との交流は、社会人の私にとっても非常に刺激となりました。また、撮影した作品を喜んでくれることもやりがいの1つです。これからもコミュニケーションをよくとっていきながら、活動の幅が広がればと思います。</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section site-section-sm">
+      <div class="container">
+        <div class="title-icon normal mb-6">代表者メッセージ</div>
+        <div class="row m-0">
+          <div class="col-12 col-md-5 text-center">
+            <img src="<?= IMG_DIR; ?>/reverend_han.jpg" class="circle-img-icon md mb-2">
+          </div>
+          <div class="col-12 col-md-7">
+            <p class="font-weight-bold small">「平和な家庭から幸せは訪れます」<br>韓総裁は家庭の大切さを若者に語りながら、若者たちが夢や志を持ち、平和な社会の実現に向けて問題解決に取り組むNGOとしてIAYSPを創設しました。IAYSPは国連経済社会理事会の特殊協議資格を有する国際NGOとして、地域が家族のように支え合う社会を目指して、プロジェクトを行っています。</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section site-section-sm">
+      <div class="container">
+        <div class="title-icon normal mb-6">Photo Gallery</div>
+        <div class="row justify-content-center">
+          <div class="col-12 col-md-4 px-3 mb-4 text-center">
+            <img src="<?= IMG_DIR; ?>/reverend_han.jpg" class="w-100">
+          </div>
+          <div class="col-12 col-md-4 px-3 mb-4 text-center">
+            <img src="<?= IMG_DIR; ?>/reverend_han.jpg" class="w-100">
+          </div>
+          <div class="col-12 col-md-4 px-3 mb-4 text-center">
+            <img src="<?= IMG_DIR; ?>/reverend_han.jpg" class="w-100">
+          </div>
+          <div class="col-12 col-md-4 px-3 mb-4 text-center">
+            <img src="<?= IMG_DIR; ?>/reverend_han.jpg" class="w-100">
+          </div>
+          <div class="col-12 col-md-4 px-3 mb-4 text-center">
+            <img src="<?= IMG_DIR; ?>/reverend_han.jpg" class="w-100">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+<?php get_footer('nomargin'); ?>
